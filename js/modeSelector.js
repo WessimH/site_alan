31e627cc88e38e2edc0661569ef0ff5ecc673c4b
WessimH <wessim.harmel1@gmail.com>
2023-11-12 23:40:45 +0100

    sss

--- js/modeSelector.js
+++ js/modeSelector.js
@@ -5,10 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
     // Choisir le thème en fonction de l'heure
     if (currentHour >= 18 || currentHour < 6) {
         // Il est 18h ou plus, ou il est moins de 6h, donc on utilise le thème sombre
+        document.getElementsByClassName("logo").src = "img/LogoDark.png";
+        console.log("dark");
         document.documentElement.setAttribute('data-theme', 'dark');
 
     } else {
         // Il est entre 6h et 18h, donc on utilise le thème clair
+        document.getElementById("logo").src = "img/LogoLight.png";
+        console.log("light");
         document.documentElement.setAttribute('data-theme', 'light');
     }
 });
